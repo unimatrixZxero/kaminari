77def2555414e5402af8a7397c522cc2045a045b
Sam Figueroa <unimatrixzxero+gravatar@gmail.com>
2011-12-07 07:47:10 +0100

    Namespace page scope

--- lib/kaminari/models/mongoid_extension.rb
+++ lib/kaminari/models/mongoid_extension.rb
@@ -19,7 +19,7 @@ module Kaminari
       included do
         # Fetch the values at the specified page number
         #   Model.page(5)
-        scope :page, Proc.new {|num|
+        scope :kaminari_page, Proc.new {|num|
           limit(default_per_page).offset(default_per_page * ([num.to_i, 1].max - 1))
         } do
           include Kaminari::MongoidCriteriaMethods
